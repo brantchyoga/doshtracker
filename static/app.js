5e49f5bb8b26abb2843915c6e7dae10ef3daad37
brantchyoga <brantrporter@gmail.com>
2018-04-12 14:54:35 -0700

    trying to deploy to heroku

--- static/app.js
+++ static/app.js
@@ -3,7 +3,8 @@
 $(document).ready(()=>{
   console.log('loaded');
   $( function() {
-      $( "#datepicker" ).datepicker();
+      $( ".datepicker" ).datepicker({ dateFormat: 'yy-mm-dd' });
+      $( ".datepicker" ).datepicker("setDate", new Date());
     } );
   // var elem = document.querySelector('.modal');
   // var instance = M.Modal.init(elem, options);
